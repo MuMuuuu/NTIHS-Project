3210261a3d82bd35ed098a4f76d347f63ad41cf7
ChaosOp <51851198+ChaosOp@users.noreply.github.com>
2020-09-28 12:17:31 +0800

    GUI Optimized

--- mobile/main.py
+++ mobile/main.py
@@ -1,23 +1,4 @@
-#!/usr/bin/python3
-from functions import send_request, setup_window
-from classes import data_set, device
-from re import *
+#! -*- coding:utf-8 -*-
+from back_port import setup_window
+setup_window()
 
-new_device = device("on")
-
-setup_window(new_device)
-
-
-try:
-    api_key = input("Input API Key : ")
-    channel = input("Input the Channel : ")
-    #result = input("Input the result : ")
-except:
-    print("\nStopped by User")
-    exit()
-
-#req_data = data_set("XXX", "read_field", 1)
-
-req_data = data_set(api_key, "write", 1, 123)
-req_data.channel_id = int(channel)
-send_request(req_data)
