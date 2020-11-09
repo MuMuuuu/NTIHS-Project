9840b94a0e8272444a92981c28c044d3b77dae1a
ChaosOp <51851198+ChaosOp@users.noreply.github.com>
2020-11-09 09:56:19 +0800

    Arduino Update "selfMQTT ver."

--- arduino/project_power_management_system/project_power_management_system.ino
+++ arduino/project_power_management_system/project_power_management_system.ino
@@ -1,78 +1,73 @@
 #include <ESP8266WiFi.h>
-WiFiClient Reading;
+#include <MQTT.h>
 
 WiFiClient client;
-String writeAPIkey ="BPP9MWS1MN6JMAJN";
-String readAPIkey ="772Y66GKM1WDJY20";
+MQTTClient mqtt;
+
 char* SSIDname = "Kev";
 char* SSIDpw = "U maniac";
+int status[4] = {0, 0, 0, 0};
 /*IPAddress ip(172,16,82,34);
 IPAddress gateway(172,16,82,254);
 IPAddress subnet(255,255,255,0);*/
-String thingSpeakValue;
-String clientDataReceive(){
-  uint8_t myData[256] = {0};
-  int i = 0;
-  while(1){
-  if(client.available()) {
-    char c = client.read();
-    myData[i++] = c;
-  }else{
-    String inData((const char*)myData);
-    return inData;
-  }
-}
-  return "";
-}
 
-String thingspeakReadData(String read_key ,int channel_id, int field_id, int record_id){
-  uint8_t myData[180] = {0};
-  int dataIdx = 0;
-  String inData = "";
-  if(client.connect("api.thingspeak.com", 80)) {
-    String things_request = "GET /channels/" + String(channel_id) + "/fields/" + String(field_id) + "/last?api_key=" + String(read_key) + "\r\n\r\n";
-    client.print(things_request);
-    delay(2000);
-    inData = clientDataReceive();
-    return inData;
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
 
+  Serial.print("\nconnecting...");
+  while (!mqtt.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(500);
+  }
 
+  Serial.println("\nconnected!");
   
+  for (int i = 1; i <= 4; i++) {
+    mqtt.subscribe(String(i) + "_writein", 2);
+  }
+
 }
 
-
-
+void message_received(String &topic, String &payload) {
+  Serial.println("get: " + topic + " : " + payload);
+  status[topic[0]] = payload;
+}
 
 void setup() {
   /*myServo.attach(5); // 伺服馬達物件連接到接腳1
   Serial.begin(9600);
   */
-    pinMode(16, OUTPUT);
-//  pinMode(10, OUTPUT);
-    Serial.begin(9600);
+  pinMode(16, OUTPUT);
+//   pinMode(10, OUTPUT);
+  Serial.begin(9600);
     
- // WiFi.config(ip,gateway,subnet);
+//   WiFi.config(ip,gateway,subnet);
   WiFi.begin(SSIDname,SSIDpw);
-  while(WiFi.status()!=WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
+  mqtt.begin("127.0.0.1", client);
+  mqtt.onMessage(message_received);
+  
+  connect();
 }
 
 void loop() {
-  int X;
-  thingSpeakValue = thingspeakReadData(readAPIkey , 1145974, 1, 0);
-  Serial.println(thingSpeakValue);
-  Reading.println(thingSpeakValue.toInt());
-  X=thingSpeakValue.toInt();
-  if(X==1){
-    digitalWrite(16,HIGH);
-    }
-   if(X==0){
-    digitalWrite(16,LOW);
-    }
+  
+  mqtt.loop();
+  
+  while (!mqtt.connected()) {
+    connect();
+  }
+  
+  //four pin write in from array status
+  
+  for (int i = 1; i <= 4; i++) {
+    mqtt.publish(String(i) + "_feedback", status[i-1], 10, 2);
+  }
+  
+
   
   /*myServo.write(thingSpeakValue.toInt);*/
   delay(1000);
